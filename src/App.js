0f21c6b42591faf81ca324fa51767ff54e351183
maksimlihoi <lihoi.maksim@gmail.com>
2020-07-31 21:05:51 +0300

    Commit after 61 lessons. Login added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Music from "./components/Music/Music";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 import {Route} from "react-router-dom";
-import Users from "./components/Users/users";
 import UsersContainer from "./components/Users/usersConteiner";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 const App = (props) => {
     return (
         <div className="app-wrapper">
-            <Header/>
+            <HeaderContainer/>
             <Navbar />
             <div className='app-wrapper-content'>
                 <Route path="/dialogs"
---
+++ src/redux/authReducer.js
@@ -0,0 +1,59 @@
+const SET_USER_DATA = "SET-USER-DATA";
+const SET_USER_PHOTO = "SET-USER-PHOTO";
+const TOGGLE_IS_FETCHING = "TOGGLE-IS-FETCHING";
+
+let initializeState = {
+    userId: null,
+    login: null,
+    email: null,
+    userPhoto: null,
+    isAuth: false,
+    isFetching: false,
+};
+
+
+const authReducer = (state = initializeState, action) => {
+
+    switch (action.type) {
+        case SET_USER_DATA: {
+            return {
+                ...state,
+                ...action.data,
+                isAuth: true,
+            };
+        }
+
+        case TOGGLE_IS_FETCHING: {
+            return {...state, isFetching: action.isFetching}
+        };
+
+        default:
+            return state;
+    }
+};
+
+export const setUserDataActionCreator = (userId, email, login) => {
+    return {
+        type: SET_USER_DATA,
+        userId: userId,
+        email: email,
+        login: login,
+    }
+};
+
+export const setUserPhotoActionCreator = (userPhoto) => {
+  return {
+      type: SET_USER_PHOTO,
+      userPhoto: userPhoto,
+  }
+};
+
+export const toggleIsFetchingActionCreator = (isFetching) => {
+    return {
+        type: TOGGLE_IS_FETCHING,
+        isFetching: isFetching,
+    };
+};
+
+
+export default authReducer;
